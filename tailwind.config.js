8d2ba9eaf4651c8a5fac6a05882a5fb910bd6d0e
Abdul <ar5951557@gmail.com>
2022-12-31 08:54:52 +0000

    html update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html"],
+  content: ["./../*.index.html"],
   theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+    },
     extend: {
       one: "url('../img/antique-cafe-bg-01.jpg')",
       two: "url('../img/antique-cafe-bg-02.jpg')",
