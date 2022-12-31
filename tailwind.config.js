174e84f2c4fa4a1e19f911807d9ff0788fbccb3f
Abdul <ar5951557@gmail.com>
2022-12-31 08:54:26 +0000

    tailwind config file update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,11 @@
 module.exports = {
   content: ["./build/*.html"],
   theme: {
-    screens: {
-      mp: "375px",
-      sm: "640px",
-    },
-
     extend: {
-      backgroundImage: {
-        hero: "url('../img/antique-cafe-bg-01.jpg')",
-        zero: "url('../img/antique-cafe-bg-02.jpg')",
-        nero: "url('../img/antique-cafe-bg-03.jpg')",
-        xero: "url('../img/antique-cafe-bg-04.jpg')",
-      },
+      one: "url('../img/antique-cafe-bg-01.jpg')",
+      two: "url('../img/antique-cafe-bg-02.jpg')",
+      three: "url('../img/antique-cafe-bg-03.jpg')",
+      four: "url('../img/antique-cafe-bg-04.jpg')",
     },
   },
   plugins: [],
